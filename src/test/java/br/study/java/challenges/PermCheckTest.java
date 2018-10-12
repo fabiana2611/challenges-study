4d557fa5b36644d89bec095ef0884e39b913b84b
fabiana <fabiana@mbp-de-fabiana.home>
2018-10-12 09:00:27 -0300

    Codility - Lesson 4 Counting Elements (test 1)

---
+++ src/test/java/br/study/java/challenges/PermCheckTest.java
@@ -0,0 +1,110 @@
+package br.study.java.challenges;
+
+import junit.framework.TestCase;
+
+public class PermCheckTest extends TestCase{
+	
+	public void testArrayEmpty() {
+		
+		int[] A = {};
+		
+		PermCheck perm = new PermCheck();
+		
+		int result = perm.solution(A);
+		
+		assertEquals(0, result);
+		
+	}
+	
+	public void testArrayNull() {
+		
+		int[] A = null;
+		
+		PermCheck perm = new PermCheck();
+		
+		int result = perm.solution(A);
+		
+		assertEquals(0, result);
+		
+	}
+	
+	public void tesIsPermutation() {
+		
+		PermCheck perm = new PermCheck();
+		
+		int[] A = {4,1,2,3};
+		
+		int result = perm.solution(A);
+		
+		assertEquals(1, result);
+	}
+	
+	public void tesIsNotPermutation() {
+		
+		PermCheck perm = new PermCheck();
+		
+		int[] A = {4,1,3};
+		
+		int result = perm.solution(A);
+		
+		assertEquals(0, result);
+	}
+	
+	public void tesIsPermutationSingleElemento() {
+		
+		PermCheck perm = new PermCheck();
+		
+		int[] A = {1};
+		
+		int result = perm.solution(A);
+		
+		assertEquals(1, result);
+	}
+	
+	public void tesIsNotPermutationSingleElemento() {
+		
+		PermCheck perm = new PermCheck();
+		
+		int[] A = {4};
+		
+		int result = perm.solution(A);
+		
+		assertEquals(0, result);
+	}
+	
+	public void tesIsNotPermutationDeplicatedElemento() {
+		
+		PermCheck perm = new PermCheck();
+		
+		int[] A = {4,1,3,1,2};
+		
+		int result = perm.solution(A);
+		
+		assertEquals(0, result);
+	}
+	
+	public void tesIsPermutationFullArray() {
+		
+		PermCheck perm = new PermCheck();
+		
+		int[] A = createArrayFull();
+		
+		int result = perm.solution(A);
+		
+		assertEquals(1, result);
+	}
+	
+	private int[] createArrayFull() {
+		
+		int N = 100000;
+		
+		int[] A = new int [N];
+		
+		for (int i = 1;i<=N;i++) {
+			A[i-1] = i;
+		}
+		
+		return A;
+	}
+
+}
